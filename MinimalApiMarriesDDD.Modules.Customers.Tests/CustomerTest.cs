dd5ba6a067ae865fe49d49600ae80231f378c51d
Alberto Acerbis <alberto.acerbis@intre.it>
2022-02-28 22:39:02 +0100

    Tests
    - Prepare Tests projects

---
+++ MinimalApiMarriesDDD.Modules.Customers.Tests/CustomerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MinimalApiMarriesDDD.Modules.Customers.Tests
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
